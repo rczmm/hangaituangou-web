45f3aa3990d6be61dd08fc071127ccaa808f5e03
rcz <3179623450@qq.com>
2025-03-05 14:04:35 +0800

    feat(config): 添加数据库配置并整合 BCryptPasswordEncoder
    - 在 application.yml 中添加了 MySQL 数据源配置- 新增 PasswordEncoderConfig 类，配置 BCryptPasswordEncoder
    - 更新 SecurityConfig 类，使用配置好的 PasswordEncoder
    - 修改 pom.xml，更新 spring-boot-starter-parent 版本

--- src/main/java/com/dsy/hangaituangou/config/SecurityConfig.java
+++ src/main/java/com/dsy/hangaituangou/config/SecurityConfig.java
@@ -1,9 +1,9 @@
 package com.dsy.hangaituangou.config;
 
-import com.dsy.hangaituangou.service.impl.UserDetailsServiceImpl;
 import jakarta.annotation.Resource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.ProviderManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
@@ -11,7 +11,6 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -20,6 +19,7 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig {
 
     @Resource
+    @Lazy
     private UserDetailsService userDetailsService;
 
     @Resource
@@ -50,11 +50,6 @@ public class SecurityConfig {
                                 .passwordParameter("password")
                 )
                 .build();
-    }
-
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
     }
 
 
