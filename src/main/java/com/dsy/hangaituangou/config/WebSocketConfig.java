36fefb80b4680c5a43c3395c9eaa0b443cb0387b
rcz <3179623450@qq.com>
2025-03-22 17:37:34 +0800

    refactor: 添加 ChatWebSocketHandler导入
    - 在 WebSocketConfig 类中添加了 ChatWebSocketHandler 的导入
    - 为后续的 WebSocket配置和处理逻辑做准备

--- src/main/java/com/dsy/hangaituangou/config/WebSocketConfig.java
+++ src/main/java/com/dsy/hangaituangou/config/WebSocketConfig.java
@@ -1,5 +1,6 @@
 package com.dsy.hangaituangou.config;
 
+import com.dsy.hangaituangou.handler.ChatWebSocketHandler;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
