f98e182e8eb5f8c6ab890be779f06ef9654424bd
rcz <3179623450@qq.com>
2025-03-22 17:35:21 +0800

    feat(mybatis-plus): 添加分页功能并更新依赖
    - 在 MybatisPlusConfig 中添加分页插件
    - 更新 pom.xml 文件，添加 mybatis-plus-bom 依赖管理
    - 移除多租户相关配置和 JSqlParser依赖
    - 更新 MyBatis-Plus 依赖版本

--- src/main/java/com/dsy/hangaituangou/config/MybatisPlusConfig.java
+++ src/main/java/com/dsy/hangaituangou/config/MybatisPlusConfig.java
@@ -1,6 +1,8 @@
 package com.dsy.hangaituangou.config;
 
+import com.baomidou.mybatisplus.annotation.DbType;
 import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,6 +12,8 @@ public class MybatisPlusConfig {
     @Bean
     public MybatisPlusInterceptor mybatisPlusInterceptor() {
         MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        // 添加分页插件
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL)); // 如果配置多个插件, 切记分页最后添加
         // 移除了多租户相关配置
         return interceptor;
     }
