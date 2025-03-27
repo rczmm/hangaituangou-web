0e01c93f87f566d5a88725dce7d044d84efb567d
rcz <3179623450@qq.com>
2025-03-27 10:33:02 +0800

    feat(search): 增加岗位类型筛选功能并优化搜索逻辑- 在 JobBO 中添加 type 和 tag 字段，用于岗位类型和标签筛选 - 重构 listByParams 方法，支持按岗位类型筛选和组合搜索 - 使用 Gson 解析 tags 字段，优化数据处理 - 更新 getTags 方法，使用 Gson 解析并返回 tag 列表

--- src/main/java/com/dsy/hangaituangou/domain/bo/JobBO.java
+++ src/main/java/com/dsy/hangaituangou/domain/bo/JobBO.java
@@ -12,4 +12,11 @@ public class JobBO extends PageBase {
 
     @Schema(description = "搜索关键字 岗位标题,岗位标签，公司", example = "Java开发工程师")
     private String keyword;
+
+    @Schema(description = "类型，岗位类型", example = "品牌推广...")
+    private String type;
+
+    @Schema(description = "标签，推荐 最新 附近", example = "recommended")
+    private String tag;
+
 }
--- src/main/java/com/dsy/hangaituangou/service/impl/JobServiceImpl.java
+++ src/main/java/com/dsy/hangaituangou/service/impl/JobServiceImpl.java
@@ -9,71 +9,77 @@ import com.dsy.hangaituangou.domain.vo.JobVO;
 import com.dsy.hangaituangou.exception.base.BusinessException;
 import com.dsy.hangaituangou.mapper.JobMapper;
 import com.dsy.hangaituangou.service.JobService;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Objects;
 import java.util.Set;
 
-/**
+ /**
  * 岗位服务实现类
  */
 @Service
 @RequiredArgsConstructor
 public class JobServiceImpl extends ServiceImpl<JobMapper, Job> implements JobService {
 
+    private final Gson gson;
+
     @Override
     public Page<JobVO> listByParams(JobBO jobBO) {
 
         Page<Job> jobPage = page(new Page<>(jobBO.getPageNum(), jobBO.getPageSize()),
                 new LambdaQueryWrapper<Job>()
-                        .like(Objects.nonNull(jobBO.getKeyword()), Job::getTitle, jobBO.getKeyword())
-                        .or()
-                        .like(Objects.nonNull(jobBO.getKeyword()), Job::getCompany, jobBO.getKeyword())
-                        .or()
-                        .like(Objects.nonNull(jobBO.getKeyword()), Job::getTags, jobBO.getKeyword())
+                        .like(Objects.nonNull(jobBO.getType()), Job::getTags, jobBO.getType())
+                        .and(Objects.nonNull(jobBO.getKeyword()) && !jobBO.getKeyword().isEmpty(), o -> o.like(Objects.nonNull(jobBO.getKeyword()),
+                                        Job::getTitle,
+                                        jobBO.getKeyword())
+                                .or()
+                                .like(Objects.nonNull(jobBO.getKeyword()), Job::getCompany, jobBO.getKeyword())
+                                .or()
+                                .like(Objects.nonNull(jobBO.getKeyword()), Job::getTags, jobBO.getKeyword()))
                         .orderByDesc(Job::getCreateTime)
         );
 
         if (jobPage != null) {
             Page<JobVO> jobVOPage = new Page<>(jobPage.getCurrent(), jobPage.getSize(), jobPage.getTotal());
-            jobVOPage.setRecords(jobPage.getRecords().stream().map(job -> JobVO.builder()
-                    .id(job.getId().toString())
-                    .title(job.getTitle())
-                    .salary(job.getSalary())
-                    .company(job.getCompany())
-                    .companySize(job.getCompanySize())
-                    .companyLogo(job.getCompanyLogo())
-                    .tags(Arrays.stream(job.getTags()
-                                    .replaceAll("\\[", "")
-                                    .replaceAll("]", "")
-                                    .replaceAll("\"", "")
-                                    .replaceAll(" ", "")
-                                    .split(","))
-                            .toList())
-                    .hrName(job.getHrName())
-                    .location(job.getLocation())
-                    .workExperience(job.getWorkExperience())
-                    .education(job.getEducation())
-                    .benefits(Arrays.stream(job.getBenefits()
-                            .replaceAll("\\[", "")
-                            .replaceAll("]", "")
-                            .replaceAll(" ", "")
-                            .replaceAll("\"", "").split(",")).toList())
-                    .description(job.getDescription())
-                    .requirements(Arrays.stream(job.getRequirements()
-                            .replaceAll("\\[", "")
-                            .replaceAll("]", "")
-                            .replaceAll(" ", "")
-                            .replaceAll("\"", "").split(",")).toList())
-                    .status(job.getStatus())
-                    .date(job.getDate())
-                    .interviewTime(job.getInterviewTime())
-                    .isFavorite(job.getIsFavorite())
-                    .build()).toList());
+            jobVOPage.setRecords(jobPage.getRecords().stream().map(job -> {
+                        List<String> tags = gson.fromJson(job.getTags(), new TypeToken<List<String>>() {
+                        }.getType());
+                        return JobVO.builder()
+                                .id(job.getId().toString())
+                                .title(job.getTitle())
+                                .salary(job.getSalary())
+                                .company(job.getCompany())
+                                .companySize(job.getCompanySize())
+                                .companyLogo(job.getCompanyLogo())
+                                .tags(tags)
+                                .hrName(job.getHrName())
+                                .location(job.getLocation())
+                                .workExperience(job.getWorkExperience())
+                                .education(job.getEducation())
+                                .benefits(Arrays.stream(job.getBenefits()
+                                        .replaceAll("\\[", "")
+                                        .replaceAll("]", "")
+                                        .replaceAll(" ", "")
+                                        .replaceAll("\"", "").split(",")).toList())
+                                .description(job.getDescription())
+                                .requirements(Arrays.stream(job.getRequirements()
+                                        .replaceAll("\\[", "")
+                                        .replaceAll("]", "")
+                                        .replaceAll(" ", "")
+                                        .replaceAll("\"", "").split(",")).toList())
+                                .status(job.getStatus())
+                                .date(job.getDate())
+                                .interviewTime(job.getInterviewTime())
+                                .isFavorite(job.getIsFavorite())
+                                .build();
+                    }
+            ).toList());
             jobVOPage.setCurrent(jobPage.getCurrent());
             jobVOPage.setSize(jobPage.getSize());
             jobVOPage.setTotal(jobPage.getTotal());
@@ -87,11 +93,9 @@ public class JobServiceImpl extends ServiceImpl<JobMapper, Job> implements JobSe
     public Object getTags() {
         Set<String> res = new HashSet<>();
         for (Job job : list()) {
-            String tags = job.getTags();
-            Collections.addAll(res, tags.replaceAll("\\[", "")
-                    .replaceAll("]", "")
-                    .replaceAll(" ", "")
-                    .replaceAll("\"", "").split(","));
+            List<String> tags = gson.fromJson(job.getTags(), new TypeToken<List<String>>() {
+            }.getType());
+            res.addAll(tags);
         }
         return res;
     }
